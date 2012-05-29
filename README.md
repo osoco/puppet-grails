60650f05beb0c3eeadf831fb541ff6cef1efb37f
deigote <diego@toharia.com>
2012-05-29 11:11:50 +0200

    Define that allows to install any version of grails

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 puppet-grails
 =============
 
-Puppet module to install any version of grails
+Puppet module to install any version of grails. 
+Depends on:
+  * Osoco's Wget puppet module: https://github.com/osoco/puppet-wget
+
+Example of usage:
+
+    class my_class_that_needs_grails {
+        grails { "grails-1.3.5":
+            version => '1.3.5',
+            destination => '/opt'
+        }
+
+        grails { "grails-2.0.0":
+            version => '2.0.0',
+            destination => '/opt'
+        }
+    }
---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+define grails ($version, $destination, $user = "root") {
+
+    include wget
+
+    if !defined(File["$destination"]) {
+        file { "$destination":
+            ensure => 'directory',
+            owner => "$user",
+            group => "$user",
+            mode => '0755'
+        }
+    }
+
+    if !defined(Package['unzip']) {
+        package { "unzip":
+            ensure => latest
+        }
+    }
+
+    if "$version" =~ /(1.3.[6-9]|2.*)/ {
+       $base_url = "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS"
+    }
+    else {
+       $base_url = "http://dist.codehaus.org/grails"
+    }
+    notify { "The base url for grails $version is '$base_url'": }
+
+    wget::fetch { "grails-$version-download":
+        source => "$base_url/grails-$version.zip",
+        destination => "$destination/grails-$version.zip",
+        require => File["$destination"]
+    }
+
+    exec { "unzip-grails-$version":
+        command => "unzip grails-$version.zip",
+        cwd => "$destination",
+        creates => "$destination/grails-$version",
+        require => [Wget::Fetch["grails-$version-download"], Package['unzip']],
+    }
+}
